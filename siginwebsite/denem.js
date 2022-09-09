645b7ccee0f2adf312411c4e5f97f6d3e59bb8f9
batuhanseren <76528441+batuhanseren@users.noreply.github.com>
2022-09-09 14:22:10 +0300

    Add files via upload

---
+++ siginwebsite/denem.js
@@ -0,0 +1,51 @@
+// alert('hello world');
+// console.log('merhaba');
+
+// var name;
+// var if;
+// var name,surname;
+
+// var let const
+
+// var x;
+// x = 'mustafa';
+// console.log(x);
+
+// let fullname = 'batuhan seren';
+// console.log(fullname);
+
+// const email = 'batuhanseren122@gmail.com';
+// console.log(email);
+
+//değişken türleri
+//primitive types
+var x;
+
+console.log(typeof x);
+
+var y = null;
+console.log(typeof y);
+
+var age = 24;
+console.log(typeof age);
+
+var online = true;
+console.log(typeof online);
+
+//reference types - objects
+//arrays
+var cars = ['volvo', 'bmw', 'mercedes'];
+console.log(typeof cars);
+var person = {
+	name: 'mustafa',
+	age: 23,
+};
+var isAlive = function () {
+	return 0;
+};
+console.log(typeof isAlive);
+//operatörler
+
+var sonuc;
+const x = 60;
+const y = 31;
